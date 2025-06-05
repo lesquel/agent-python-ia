df92e5aeb7bf0b0dd82716369064ff2c76ad6f40
lesquel <lesquel1319@gmail.com>
2025-06-05 07:43:41 -0500

    Revert "feature(site) basic components, sidebar - input-text and chat-buttons"
    This reverts commit ee78e1fd7920c863499043a128a00cc0aea1b3a7.

--- fronted/src/app/modules/site/pages/home/home.ts
+++ fronted/src/app/modules/site/pages/home/home.ts
@@ -1,23 +1,13 @@
-// import { RouterLink } from '@angular/router';
-// import { siteConfigRoutes } from '../../config/site-config.routes';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { RouterLink } from '@angular/router';
+import { siteConfigRoutes } from '../../config/site-config.routes';
 import { AgentList } from "../../../agent/components/agent-list/agent-list";
-import { SidebarComponent } from "../../components/sidebar/sidebar";
-import { InputText } from "../../components/input-text/input-text";
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [
-    AgentList,
-    SidebarComponent,
-    CommonModule,
-    InputText
-  ],
+  imports: [ AgentList],
   templateUrl: './home.html'
 })
-
 export class Home {
-  sidebarVisible = true;
+  protected siteRoutes = siteConfigRoutes;
 }
